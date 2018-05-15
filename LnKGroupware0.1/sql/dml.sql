a1ee4c4d0b6e5f4e256d559333fcad1fd0d89d18
KOITT <KOITT@KOITT-PC>
2018-05-15 13:33:01 +0900

    dept테이블 INSERT 작성함

--- LnKGroupware0.1/sql/dml.sql
+++ LnKGroupware0.1/sql/dml.sql
@@ -1,7 +1,17 @@
-# 그룹웨어 사용 권한 입력 (관리자 / 사용자)
+# dept 부서 입력
+INSERT INTO dept (dpet_no, dept_name) VALUES (10, '경영지원부');
+INSERT INTO dept (dpet_no, dept_name) VALUES (11, '영업부');
+INSERT INTO dept (dpet_no, dept_name) VALUES (12, '개발부');
+INSERT INTO dept (dpet_no, dept_name) VALUES (13, '마케팅부');
+INSERT INTO dept (dpet_no, dept_name) VALUES (14, '고객지원부');
+
+
+# grade에 그룹웨어 사용 권한 입력 (관리자 / 사용자)
 INSERT INTO grade (grade_no, grade_name) VALUES (100, 'MASTER');
 INSERT INTO grade (grade_no, grade_name) VALUES (200, 'CEO');
 INSERT INTO grade (grade_no, grade_name) VALUES (201, 'DIRECTOR');
 INSERT INTO grade (grade_no, grade_name) VALUES (202, 'HEAD');
 INSERT INTO grade (grade_no, grade_name) VALUES (203, 'CHEIF');
 INSERT INTO grade (grade_no, grade_name) VALUES (204, 'STAFF');
+
+
